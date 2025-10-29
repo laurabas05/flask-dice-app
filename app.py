6114a77b3941977792fd9dc7787f1f38941c3880
diegaless <laaurabas@gmail.com>
2025-10-29 22:45:03 +0100

    Resuelto el ejercicio dice-app

--- app.py
+++ app.py
@@ -8,16 +8,21 @@ app = Flask(__name__)
 def get_sides():
     """Return how many sides the die should have based on the DICE_SIDES env var."""
     # TODO: leer DICE_SIDES desde la variable de entorno (usa os.getenv como base)
+    dice_sides = os.getenv("DICE_SIDES", "6")
     # TODO: convertirlo a int, validar que sea >= 2 y devolverlo
     # TODO: si la variable no existe o es invalida, devolver 6
-    return 6  # placeholder para que la app arranque
+    sides = int(dice_sides)
+    if (sides >= 2):
+        return sides
+    else:
+        return 6  # placeholder para que la app arranque
 
 
 @app.route("/")
 def index():
     sides = get_sides()
     # TODO: generar un numero aleatorio entre 1 y `sides`
-    result = 1
+    result = random.randint(1, sides)
     return render_template("index.html", result=result, sides=sides)
 
 
